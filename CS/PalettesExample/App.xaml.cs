7edfca3e10d1401a09f32ea028864f5efa9054d4
Sameishchev Anton <anton.sameishchev@devexpress.com>
2019-07-11 15:34:03 +0300

    add example content

---
+++ CS/PalettesExample/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using DevExpress.Xpf.Core;
+using System.Windows;
+
+namespace PalettesExample
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application{
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            Theme.RegisterPredefinedPaletteThemes();
+            ApplicationThemeHelper.ApplicationThemeName = PredefinedThemePalettes.RedWine.Name + Theme.Office2019White.Name;
+            base.OnStartup(e);
+        }
+    }
+}
---
+++ CS/PalettesExample/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using DevExpress.Xpf.Core;
+using System.Windows;
+using System.Windows.Media;
+
+namespace PalettesExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : DevExpress.Xpf.Core.ThemedWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var custompalette = new ThemePalette("CustomPalette");
+            custompalette.SetColor("Foreground", (Color)ColorConverter.ConvertFromString("#FFFF7200"));
+            custompalette.SetColor("Backstage.Focused", Colors.White);
+            var customtheme = Theme.CreateTheme(custompalette, Theme.Office2016ColorfulSE);
+            Theme.RegisterTheme(customtheme);
+            ApplicationThemeHelper.ApplicationThemeName = customtheme.Name;
+        }
+    }
+}
