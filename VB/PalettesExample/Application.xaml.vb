b8387b74bd10a74d77df5cde60ca9e91ba7cd35e
DevExpressExampleBot <fakeEmail>
2019-07-12 11:37:09 +0300

    Source auto update [skip ci]

---
+++ VB/PalettesExample/Application.xaml.vb
@@ -0,0 +1,17 @@
+﻿Imports DevExpress.Xpf.Core
+Imports System.Windows
+
+Namespace PalettesExample
+	''' <summary>
+	''' Interaction logic for App.xaml
+	''' </summary>
+	Partial Public Class App
+		Inherits Application
+
+		Protected Overrides Sub OnStartup(ByVal e As StartupEventArgs)
+			Theme.RegisterPredefinedPaletteThemes()
+			ApplicationThemeHelper.ApplicationThemeName = PredefinedThemePalettes.RedWine.Name + Theme.Office2019White.Name
+			MyBase.OnStartup(e)
+		End Sub
+	End Class
+End Namespace
